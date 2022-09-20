e81aea7b19c087c6564c179d42e339189a303adc
Brian <brianfoody@gmail.com>
2022-09-20 11:38:41 +0800

    Update paste.ts
    Remove comments

--- examples/core-example-advanced/src/state/actions/selection/paste.ts
+++ examples/core-example-advanced/src/state/actions/selection/paste.ts
@@ -5,10 +5,6 @@ export const paste: Action = (data) => {
   const selectedShapes = Object.values(data.page.shapes).filter((shape) => shape.isCopied)
 
   const newShapes = selectedShapes.map((s) => ({ ...s, id: nanoid() }))
-  console.log('oldshape ids')
-  console.log(selectedShapes.map((s) => s.id))
-  console.log('newshape ids')
-  console.log(newShapes.map((s) => s.id))
 
   for (const shape of newShapes) {
     data.page.shapes[shape.id] = shape
